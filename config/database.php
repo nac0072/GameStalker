af0e284827bbf4e2e48744555d3f103c9e671369
unknown <Tyler@.(none)>
2012-02-14 13:53:45 -0600

    Change to database and paths for local use

--- config/database.php
+++ config/database.php
@@ -5,3 +5,4 @@ define('DB_HOST', 'localhost');
 define('DB_NAME', 'userdatabase');
 define('DB_USER', 'root');
 define('DB_PASS', 'root');
+//define('DB_PASS', '');
--- config/paths.php
+++ config/paths.php
@@ -3,6 +3,6 @@
 // Always provide a TRAILING SLASH (/) AFTER A PATH
 
 define('URL', 'http://gamestalker.dyndns.org/');
-
+//define('URL', 'http://localhost/gamestalker/');
 
 define('LIBS', 'libs/');
