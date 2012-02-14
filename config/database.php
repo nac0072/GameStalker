3b891ffecee5c45a0f20582107726d1f3f49f3fb
Andrew <agbishop@tx.rr.com>
2012-02-14 14:39:24 -0600

    fixed the paths and db config

--- config/database.php
+++ config/database.php
@@ -4,9 +4,6 @@ define('DB_TYPE', 'mysql');
 define('DB_HOST', 'localhost');
 define('DB_NAME', 'userdatabase');
 define('DB_USER', 'root');
-<<<<<<< HEAD
-define('DB_PASS', '');
-=======
+//define('DB_PASS', '');
 define('DB_PASS', 'root');
 //define('DB_PASS', '');
->>>>>>> 4ffe3b2a2c703bd733d0f5e3d24faaccc4c9c32e
